c33f598f133934d4e7e8d05eea65c608faaaea33
wonjong <bjlaancgk@naver.com>
2021-12-13 17:10:55 +0900

    211213

--- Baekjoon/16928.py
+++ Baekjoon/16928.py
@@ -1,47 +1,58 @@
 import sys
 sys.stdin = open('input.txt')
-sys.setrecursionlimit(10 ** 6)
+from collections import deque
 
 
-def check_ladder(x):
+def check_ladder(x):                # 사다리 체크
     for i in range(len(ladder)):
         if x == ladder[i][0]:
             return ladder[i][1]
     return 0
 
-def check_snake(x):
+def check_snake(x):                 # 뱀 체크
     for i in range(len(snake)):
         if x == snake[i][0]:
             return snake[i][1]
     return 0
 
-def sol(x, cnt):
-    print(x, cnt)
-    if x >= 100:
-        return
+def sol(s, e):
+    global min_cnt
 
-    for i in range(1, 7):
-        if check_ladder(x+i):
-            sol(check_ladder(x+i), cnt+1)
-        elif check_snake(x+i):
-            continue
-        else:
-            sol(x+i, cnt+1)
+    q = deque()
+    q.append((s, 0))    # 시작 위치 저장
+    visited[s] = 1      # 방문
+
+    while q:
+        t, c = q.popleft()
+
+        if t == e:      # 도착
+            min_cnt = min(min_cnt, c)
+
+        for d in range(1, 7):       # 주사위 굴려서 갈 수 있는 모든 거리
+            nt = t + d
+            nc = c + 1
+            up = check_ladder(nt)   # 사다리 있을 때 이동할 위치 저장
+            down = check_snake(nt)  # 뱀 있을 때 이동할 위치 저장
+
+            if nt <= 100 and visited[nt] == 0:  # 100 이하, 방문하지 않았으면
+                visited[nt] = 1                 # 방문
+                if up:                          # 방문한 곳이 사다리일 때
+                    q.append((up, nc))
+                elif down:                      # 방문한 곳이 뱀일 때
+                    q.append((down, nc))
+                else:
+                    q.append((nt, nc))
 
 N, M = map(int, input().split())    # N: 사다리 수, M: 뱀 수
 ladder = [list(map(int, input().split())) for _ in range(N)]
 snake = [list(map(int, input().split())) for _ in range(M)]
 
-max_
-for i in range(len(ladder)):
+start = 1
+end = 100
 
+visited = [0] * 101     # 방문 표시
+min_cnt = 100000        # 주사위 굴린 횟수 최소값
 
-print(ladder)
-print(snake)
+sol(start, end)
 
-# sol(1, 0)
-
-# ladder.sort()
-#
-# print(ladder)
-
+print(min_cnt)
