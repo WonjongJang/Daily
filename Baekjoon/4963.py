de3571f7e97e297ba2e9d4b9ab36d106d6214f3f
wonjong <bjlaancgk@naver.com>
2022-07-16 01:22:00 +0900

    algo

--- Baekjoon/4963.py
+++ Baekjoon/4963.py
@@ -1,5 +1,40 @@
 import sys
 sys.stdin = open('input.txt')
+sys.setrecursionlimit(10**6)
+
+
+dx = [-1, 0, 1, 0, -1, -1, 1, 1]
+dy = [0, 1, 0, -1, -1, 1, -1, 1]
+
+def recur(x, y):
+    arr[x][y] = 2
+
+    for d in range(8):
+        nx = x + dx[d]
+        ny = y + dy[d]
+        if 0 <= nx < h and 0 <= ny < w and arr[nx][ny] == 1:
+            recur(nx, ny)
+
+while True:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break
+    arr = [list(map(int, input().split())) for _ in range(h)]
+
+    cnt = 0
+    for x in range(h):
+        for y in range(w):
+            if arr[x][y] == 1:
+                cnt += 1
+                recur(x, y)
+
+    print(cnt)
+
+
+
+'''
+import sys
+sys.stdin = open('input.txt')
 
 
 # 상하좌우 대각선
@@ -36,3 +71,4 @@ while True:
                 cnt += 1            # 섬 개수 증가
 
     print(cnt)
+'''
