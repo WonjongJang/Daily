92c7345f5bb2f1c054d7a77905f5734b0b8f2fe1
wonjong <bjlaancgk@naver.com>
2022-10-01 11:26:17 +0900

    algo

--- inflearn/파이썬 알고리즘 문제풀이/8-4 최대 부분 증가수열.py
+++ inflearn/파이썬 알고리즘 문제풀이/8-4 최대 부분 증가수열.py
@@ -7,11 +7,35 @@ for tc in range(1, T+1):
     N = int(input())
     data = list(map(int, input().split()))
 
-    data.sort()
-    print(data)
+    dp = [0]*N
+    dp[0] = 1
+    for i in range(1, N):
+        max_v = 0
+        for j in range(i-1, -1, -1):
+            if data[j] < data[i] and dp[j] > max_v:
+                max_v = dp[j]
+        dp[i] = max_v + 1
+
+    print(max(dp))
 
 
 
 ''' [풀이]
-
+import sys
+sys.stdin = open("input.txt", 'r')
+n=int(input())
+arr=list(map(int, input().split()))
+arr.insert(0,0)
+dy=[0]*(n+1)
+dy[1]=1
+res=0
+for i in range(2, n+1):
+    max=0
+    for j in range(i-1, 0, -1):
+        if arr[j]<arr[i] and dy[j]>max:
+            max=dy[j]
+    dy[i]=max+1
+    if dy[i]>res:
+        res=dy[i]
+print(res)
 '''
