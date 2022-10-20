e1dae4d394a2c8dac111845963002b11b69a9f38
wonjong <bjlaancgk@naver.com>
2022-10-20 23:37:23 +0900

    algo

--- Baekjoon/2293.py
+++ Baekjoon/2293.py
@@ -4,9 +4,11 @@ sys.stdin = open('input.txt')
 
 N, K = map(int, input().split())
 dp = [0]*(K+1)
+dp[0] = 1
 for _ in range(N):
-    w, k = map(int, input().split())
-    for i in range(K, w-1, -1):
-        dp[i] = max(dp[i], dp[i-w]+k)
+    c = int(input())
+    for i in range(c, K+1):
+        if i-c >= 0:
+            dp[i] += dp[i-c]
 
 print(dp[K])
