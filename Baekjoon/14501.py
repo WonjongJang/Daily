95d6be99e038aefb780b3567800c92b1b7337215
wonjong <bjlaancgk@naver.com>
2022-10-22 11:31:48 +0900

    algo

--- Baekjoon/14501.py
+++ Baekjoon/14501.py
@@ -2,39 +2,54 @@ import sys
 sys.stdin = open('input.txt')
 
 
+def recur(L, sum_p):
+    global max_v
+    sum_p += P[L]
+    print(L)
+
+    for i in range(adj[L], N+1):
+        if i <= N and adj[i] <= N+1:
+            recur(i, sum_p)
+
+    print(sum_p)
+    max_v = max(max_v, sum_p)
+
 N = int(input())
-adj = [[0]*(N+1) for _ in range(N+1)]
+adj = [0]*(N+1)
 P = [0]*(N+1)
 for i in range(1, N+1):
     t, p = map(int, input().split())
 
-    if i+t <= N:
-        adj[i][i+t] = 1
+    adj[i] = i+t
     P[i] = p
 
 print(adj)
 print(P)
+max_v = 0
+for s in range(1, N+1):
+    if adj[s] <= N+1:
+        recur(s, 0)
 
-# def recur(L, sum):
-#     global max_v
-#     print(L, sum)
-#     if L+data[L][0] > N:
-#         max_v = max(max_v, sum)
-#     else:
-#         recur(L+data[L][0], sum+data[L][1])
-#
-# N = int(input())
-# data = [[0, 0]]
-# for _ in range(N):
-#     t, p = map(int, input().split())
-#     data.append([t, p])
-#
-# print(data)
-# max_v = 0
-# for i in range(1, N+1):
-#     t = data[i][0]
-#     p = data[i][1]
-#     if i+t <= N:
-#         recur(i, p)
-#
-# print(max_v)
+print(max_v)
+
+
+
+''' dp 풀이
+n = int(input())
+t = []
+p = []
+dp = []
+for i in range(n):
+    a, b = map(int, input().split())
+    t.append(a)
+    p.append(b)
+    dp.append(b)
+dp.append(0)
+print(dp)
+for i in range(n - 1, -1, -1):
+    if t[i] + i > n:
+        dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(dp[i + 1], p[i] + dp[i + t[i]])
+print(dp)
+'''
