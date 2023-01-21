d47ff8ed10d6fca36d85b99a457dd18a69b4e2b6
wonjongjang <bjlaancgk@naver.com>
2023-01-21 23:02:08 +0900

    [BOJ] 쉬운 계단 수 23.01.21

--- Baekjoon/10844.py
+++ Baekjoon/10844.py
@@ -3,13 +3,22 @@ sys.stdin = open('input.txt')
 
 
 N = int(input())
-number = [list(range(1, 10))]
+dp = [[0]*10 for _ in range(N+1)]
 
-print(number)
-for n in number:
+# 마지막 자리가 0~9로 끝나는 것의 개수로 규칙 찾음 (좌우 대각선 위의 값을 합친 것)
+for i in range(1, 10):  # N=1, 즉 한자리 수일 때 1에서 9까지 1개씩 초기화 (0으로 시작할 수 없기 때문에 한자리 수는 0 제외)
+    dp[1][i] = 1
 
+for i in range(2, N+1): # 두자리 수 부터 N자리 수 까지
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][j+1] # 오른쪽 위 대각선 밖에 없음
+        elif j == 9:
+            dp[i][j] = dp[i-1][j-1] # 왼쪽 위 대각선 밖에 없음
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]  # 좌우 대각선 위
 
-
+print(sum(dp[N]) % 1000000000)
 
 
 
