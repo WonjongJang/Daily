f1a481ca68a5bad61d9a04469ae44e03bd499ed1
wonjongjang <bjlaancgk@naver.com>
2023-02-06 20:50:40 +0900

    [BOJ] 2xn 타일링 23.02.06

--- Baekjoon/11726.py
+++ Baekjoon/11726.py
@@ -2,16 +2,29 @@ import sys
 sys.stdin = open('input.txt')
 
 
-N = int(input())
-
-if N > 2:
-    dp = [0] * (N+1)
-
+n = int(input())
+if n > 2:
+    dp = [0]*(n+1)
     dp[1] = 1
     dp[2] = 2
-    for i in range(3, N+1):
-        dp[i] = dp[i-1] + dp[i-2]
-
-    print(dp[N] % 10007)
+    for i in range(3, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007
+    print(dp[n])
 else:
-    print(N)
+    print(n)
+
+
+
+# N = int(input())
+#
+# if N > 2:
+#     dp = [0] * (N+1)
+#
+#     dp[1] = 1
+#     dp[2] = 2
+#     for i in range(3, N+1):
+#         dp[i] = dp[i-1] + dp[i-2]
+#
+#     print(dp[N] % 10007)
+# else:
+#     print(N)
