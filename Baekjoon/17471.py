86841b1fe62b0b758656e5bc687b90984100308a
wonjongjang <bjlaancgk@gmail.com>
2023-03-14 20:35:49 +0900

    [BOJ] 게리맨더링 23.03.14

--- Baekjoon/17471.py
+++ Baekjoon/17471.py
@@ -2,69 +2,63 @@ import sys
 sys.stdin = open('input.txt')
 
 
-def adj_check(arr):
-    q = []
-    visited = [0] * (N+1)
+def bfs(arr):
+    que = [arr[0]]
+    v = [0] * (N+1) # 방문 처리
+    v[arr[0]] = 1
 
-    q.append(arr[0])
-    visited[arr[0]] = 1
-    cnt = 1
-    sum = 0
-    while q:
-        x = q.pop(0)
-        sum += population[x]
-        for f in range(N+1):
-            if adj[x][f] == 1 and f in arr and visited[f] == 0:
-                cnt += 1
+    cnt = 0
+    sum = 0 # 인구 합계
+    while que:
+        x = que.pop(0)  # x 구역
+        cnt += 1
+        sum += P[x]
+        for i in adj[x]:    # x 구역과 인접한 구역
+            if i in arr and not v[i]:
+                que.append(i)
+                v[i] = 1
+
     if cnt == len(arr):
         return sum
-    else:
+    else:   # cnt와 arr 길이가 맞지 않는다면 나눈 선거구의 구역들이 인접하지 않은 것
         return 0
 
+
+
+def dfs(x): # 부분 집합 만드는 함수
+    global min_v
+
+    if x == N+1:
+        A, B = [], []   # A, B 선거구
+        for i in range(1, len(S)):
+            if S[i]:
+                A.append(i)
+            else:
+                B.append(i)
+
+        if len(A) and len(B):   # 선거구는 구역을 적어도 하나 포함해야 함 (공집합 X)
+            a = bfs(A)  # A 선거구 인구 합계
+            b = bfs(B)  # B 선거구 인구 합계
+            if a and b:
+                min_v = min(min_v, abs(a-b))
+    else:
+        S[x] = 1
+        dfs(x+1)
+        S[x] = 0
+        dfs(x+1)
+
 N = int(input())    # 구역의 개수
-population = [0] + list(map(int, input().split()))    # 인구
+P = [0] + list(map(int, input().split()))   # 각 구역의 인구
+adj = [[]]  # 각 구역과 인접한 구역
+for _ in range(N):
+    temp = list(map(int, input().split()))
+    adj.append(temp[1:])
 
-adj = [[] for _ in range(N+1)]   # 인접 행렬
-ans = 0
-for n in range(1, N+1):
-    data = list(map(int, input().split()))
+min_v = 100000  # 최소값 초기화
+S = [0] * (N+1) # 부분 집합
+dfs(1)
 
-    # for d in range(1, len(data)):
-    #     adj[n][data[d]] = 1
-
-print(adj)
-
-area = list(range(1, N+1))
-min_v = 100000
-
-for i in range(1, (1<<len(area))):
-    t_sum = 0
-    temp1 = []
-    temp2 = []
-    for j in range(0, len(area)):
-        if i & (1<<j):
-            # print(r[j], end=' ')
-            t_sum += population[area[j]]
-            temp1.append(area[j])
-        else:
-            temp2.append(area[j])
-    print()
-
-    print('sum', t_sum)
-    print(temp1)
-    print(temp2)
-
-    a1 = adj_check(temp1)
-
-    if not a1:
-        continue
-
-    a2 = adj_check(temp2)
-
-    if not a2:
-        continue
-
-    min_v = min(min_v, abs(a1-a2))
-
-# 출력
-print(min_v)
+if min_v == 100000: # 두 선거구로 나눌 수 있는 방법이 없는 경우
+    print(-1)
+else:
+    print(min_v)
